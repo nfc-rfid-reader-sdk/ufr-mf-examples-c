b42d570dd57f63415c05c8d5725b1f2770c417eb
developer3 <support@d-logic.rs>
2017-11-24 11:30:09 +0100

    Add helper function to call ReaderOpenEx() and print status

--- main.c
+++ main.c
@@ -13,6 +13,20 @@
 
 #include "lib/include/uFCoder.h"
 
+/**
+ * Calls ReaderOpenEx() and print status
+ */
+UFR_STATUS reader_open_ex(uint32_t reader_type, c_string port_name, uint32_t port_interface)
+{
+	UFR_STATUS status;
+
+	status = ReaderOpenEx(reader_type, port_name, port_interface, 0);
+	printf("ReaderOpenEx(%d, '%s', %d, 0):> %s\n", reader_type, port_name,
+			port_interface, UFR_Status2String(status));
+
+	return status;
+}
+
 int main(void)
 {
 	UFR_STATUS status;
@@ -22,9 +36,7 @@ int main(void)
 
 	puts("Test opening Digital Logic shield uFR RS232 on RPi: 1.0\n");
 
-	status = ReaderOpenEx(2, "/dev/serial0", 10, 0);
-	printf("ReaderOpenEx(2, '/dev/serial0', 10, 0):> %s\n",
-			UFR_Status2String(status));
+	status = reader_open_ex(2, "/dev/serial0", 10);
 
 	status = ReaderUISignal(3, 3);
 	printf("ReaderUISignal(3, 3):> %s\n", UFR_Status2String(status));
